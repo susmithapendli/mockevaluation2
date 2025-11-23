64ca65cbadff07a584df831e76e204ba89dd2237
susmithapendli <susmithapendli7@gmail.com>
2025-11-23 19:14:10 +0530

    initial setup

---
+++ evaluation.js
@@ -0,0 +1,8 @@
+let arr1 = [10, 20, 30];
+let arr2 = [40, 50];
+let combined=[...arr1, ...arr2];
+console.log("Q1 - Combined array:", combined);
+
+
+let person = { name: "Venu", age: 25 };
+let extra = { city: "Bengaluru" };
